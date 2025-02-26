38497a313d3d3b3da182601db00dbf09764c1cd9
Dnyaneshwar <palkemauli1022@gmail.com>
2025-02-26 10:40:53 +0530

    Solved : LeetCode Que No.(2520) Count the Digits That Divide a Number (26-02-2025)

---
+++ Questions/CountDigit.java
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/count-the-digits-that-divide-a-number/description/
+public class CountDigit {
+    public static void main(String[] args) {
+        int num = 121;
+        System.out.println(countDigits(num));
+    }
+
+    static int countDigits(int num) {
+        int count = 0;
+        int temp = num;
+        while (temp != 0) {
+            int n = temp % 10;
+
+            if (num % n == 0) {
+                count++;
+            }
+            temp /= 10;
+        }
+
+        return count;
+    }
+}
